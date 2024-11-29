f973b3f15450754142d00bcc6a8860f0da54f51f
aknEvrnky <evrenkaya.2002.save@gmail.com>
2024-11-28 20:42:56 -0500

    Make Gender as enum

---
+++ app/Gender.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+enum Gender: string
+{
+    case Men = 'men';
+    case Women = 'women';
+    case Unisex = 'unisex';
+    case Boys = 'boys';
+    case Girls = 'girls';
+}
--- tests/Unit/Models/GenderTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Models\Gender;
-
-it('can be created', function () {
-    $gender = Gender::factory()->create();
-    expect($gender->id)->toBe(1);
-});
